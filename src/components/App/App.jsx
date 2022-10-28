21e58e79588529d482cea7e9ea0968ef58d3866a
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-10-27 21:26:32 -0700

    install prop-types, nanoid, made App file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,29 @@
+// import { render } from "@testing-library/react";
+// import React, {Component } from "react";
+
+
+
+// class App extends Component = () => {
+// state = {
+//   contacts: [],
+//   filter: '',
+//   name: '',
+//   number: ''
+// }
+
+
+
+//   render(){
+//     //
+//   return (
+//     <>
+    
+//     </>
+
+
+    
+//   );
+// }
+
+// };
+// export default App;
